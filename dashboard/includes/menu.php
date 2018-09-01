a5d91053d2d6ca2bd15c7f75ae66cfbc65a0b8f8
godofping <rexmicrosoft@yahoo.com.ph>
2018-09-01 17:54:11 +0800

    up

--- dashboard/includes/menu.php
+++ dashboard/includes/menu.php
@@ -1,1 +1,18 @@
-menu
+<!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                         <li class="nav-devider"></li>
+                        <li class="nav-small-cap">Menu</li>
+
+                        <li class="active"> <a class="waves-effect waves-dark"><i class="mdi mdi-gauge"></i>Dashboard</a></li>
+
+                        <li> <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><i class="mdi mdi-settings"></i><span class="hide-menu">Settings</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="index.html">Minimal </a></li>
+                            </ul>
+                        </li>
+
+                                             
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
