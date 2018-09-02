e58dab50f7f6fd2ce129dc6b9618156575886ac1
godofping <rexmicrosoft@yahoo.com.ph>
2018-09-02 09:14:04 +0800

    update-icons

--- dashboard/includes/menu.php
+++ dashboard/includes/menu.php
@@ -6,9 +6,9 @@
 
                         <li> <a href="home.php" class="waves-effect waves-dark"><i class="mdi mdi-gauge"></i>Dashboard</a></li>
 
-                        <li> <a href="booking-schedules.php" class="waves-effect waves-dark"><i class="mdi mdi-gauge"></i>Booking Schedules</a></li>
+                        <li> <a href="booking-schedules.php" class="waves-effect waves-dark"><i class="mdi mdi-calendar"></i>Booking Schedules</a></li>
 
-                        <li> <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><i class="mdi mdi-settings"></i><span class="hide-menu">Bookings</span></a>
+                        <li> <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><i class="mdi mdi-calendar-clock"></i><span class="hide-menu">Bookings</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="travel-and-tour.php">Travel and Tour</a></li>
                                 <li><a href="van-rentals.php">Van Rentals</a></li>
@@ -16,27 +16,27 @@
                             </ul>
                         </li>
 
-                        <li><a class="has-arrow waves-effect waves-dark" aria-expanded="false"><i class="mdi mdi-settings"></i><span class="hide-menu">Services</span></a>
+                        <li><a class="has-arrow waves-effect waves-dark" aria-expanded="false"><i class="mdi mdi-washing-machine"></i><span class="hide-menu">Services</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="packages.php">Packages</a></li>
                                 <li><a href="vans.php">Vans</a></li>
                             </ul>
                         </li>
 
-                        <li> <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><i class="mdi mdi-settings"></i><span class="hide-menu">Customers</span></a>
+                        <li> <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><i class="mdi mdi-account-multiple-outline"></i><span class="hide-menu">Customers</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="walk-in-customers.php">Walk-in Customers</a></li>
                                 <li><a href="online-customers.php">Online Customers</a></li>
                             </ul>
                         </li>
 
-                        <li> <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><i class="mdi mdi-settings"></i><span class="hide-menu">Billings</span></a>
+                        <li> <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><i class="mdi mdi-book-open-variant"></i><span class="hide-menu">Billings</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="payment-transactions.php">Payment Transactions</a></li>
                             </ul>
                         </li>
 
-                        <li> <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><i class="mdi mdi-settings"></i><span class="hide-menu">Reports</span></a>
+                        <li> <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><i class="mdi mdi-library-books"></i><span class="hide-menu">Reports</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="bookings-schedules.php">Booking Schedules</a></li>
                                 <li><a href="customer-information.php">Customer Information</a></li>
