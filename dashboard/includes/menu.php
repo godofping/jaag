fb4cb23073e754871e55bd6b59bb004016b5f6f1
godofping <rexmicrosoft@yahoo.com.ph>
2018-10-05 09:55:37 +0800

    update font

--- dashboard/includes/menu.php
+++ dashboard/includes/menu.php
@@ -1,17 +1,17 @@
 <!-- Sidebar navigation-->
                 <nav class="sidebar-nav">
-                    <ul id="sidebarnav">
+                    <ul id="sidebarnav" >
                      
                         <li class="nav-small-cap">Menu</li>
 
-                        <li> <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-home"></i><span class="hide-menu">Home</span></a>
+                        <li > <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-home" style="color: black !important;"></i><span class="hide-menu">Home</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="home.php">Dashboard</a></li>
           
                             </ul>
                         </li>
 
-                        <li> <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-calendar"></i><span class="hide-menu">Schedules</span></a>
+                        <li> <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-calendar" style="color: black !important;"></i><span class="hide-menu">Schedules</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="booking-schedules.php">Booking Schedules</a></li>
           
@@ -19,7 +19,7 @@
                         </li>
 
 
-                        <li> <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-calendar-clock"></i><span class="hide-menu">Bookings</span></a>
+                        <li> <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-calendar-clock" style="color: black !important;"></i><span class="hide-menu" >Bookings</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="add-booking.php">Add Booking</a></li>
                                 <li><a href="travel-and-tour.php">Travel and Tour</a></li>
@@ -27,14 +27,14 @@
                             </ul>
                         </li>
 
-                        <li><a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-washing-machine"></i><span class="hide-menu">Services</span></a>
+                        <li><a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-washing-machine" style="color: black !important;"></i><span class="hide-menu">Services</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="packages.php">Packages</a></li>
                               
                             </ul>
                         </li>
 
-                        <li> <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-account-multiple-outline"></i><span class="hide-menu">Customers</span></a>
+                        <li> <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-account-multiple-outline" style="color: black !important;"></i><span class="hide-menu">Customers</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="walk-in-customers.php">Walk-in Customers</a></li>
                                 <li><a href="online-customers.php">Online Customers</a></li>
@@ -42,14 +42,14 @@
                         </li>
 
                         <?php if ($_SESSION['accountType'] == 'Administrator'): ?>
-                            <li> <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-book-open-variant"></i><span class="hide-menu">Billings</span></a>
+                            <li> <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-book-open-variant" style="color: black !important;"></i><span class="hide-menu">Billings</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="payment-transactions.php">Payment Transactions</a></li>
                             </ul>
                         </li>
                         <?php endif ?>
 
-                        <li> <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-library-books"></i><span class="hide-menu">Reports</span></a>
+                        <li> <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-library-books" style="color: black !important;"></i><span class="hide-menu">Reports</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="bookings-schedules.php">Booking Schedules</a></li>
                                 <li><a href="list-of-travelers.php">List of Travelers</a></li>
@@ -62,7 +62,7 @@
                         </li>
 
                         <?php if ($_SESSION['accountType'] == 'Administrator'): ?>
-                            <li> <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-settings"></i><span class="hide-menu">Settings</span></a>
+                            <li> <a class="has-arrow waves-effect " aria-expanded="false"><i class="mdi mdi-settings" style="color: black !important;"></i><span class="hide-menu">Settings</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="attendants.php">Attendants</a></li>
                                 <li><a href="places.php">Places</a></li>
