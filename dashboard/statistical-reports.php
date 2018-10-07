7356e44045e968bc69f9b3ef5b5f9509a8b5b3e1
godofping <rexmicrosoft@yahoo.com.ph>
2018-10-07 21:08:09 +0800

    Update dashboard/statistical-reports.php

--- dashboard/statistical-reports.php
+++ dashboard/statistical-reports.php
@@ -27,15 +27,125 @@ include("includes/header.php");
                 <!-- ============================================================== -->
                 <!-- Start Page Content -->
                 <!-- ============================================================== -->
+                <?php if (isset($_GET['month']) and isset($_GET['packageId'])): ?>
+                <a href="print/print-report-payment-transactions.php?month=<?php echo $_GET['month'] ?>&packageId=<?php echo $_GET['packageId'] ?>" target="blank"><button class="btn btn-info btn-sm mr5"  style="margin-bottom: 20px;">Print</button></a> 
+                <?php endif ?>
+                
+
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
                             <div class="card-body">
-                                <div id="morris-area-chart2" style="height: 405px;"></div>
+                    <form method="GET">
 
-
-                                
+                    <div class="row">
+                        <div class="col-md-4">
+                            <label>Month</label>
+                            <div class="form-group">
+                                <input class="form-control" type="month" name="month"  value="<?php echo date('Y-m') ?>">
                             </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <label>Package Name</label>
+                            <div class="form-group">
+                                <select class="form-control" name="packageId">
+                                    <option value="">All package</option>
+                                    <?php
+                                    $qry = mysqli_query($connection,"select * from package_view");
+                                    while ($res = mysqli_fetch_assoc($qry)) { ?>
+                                        <option value="<?php echo $res['packageId'] ?>"><?php echo $res['packageName']; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <button class="btn btn-success m-t-20 waves-effect text-left">Search</button>
+                        </div>
+                    </div>
+
+                </form>
+
+                    <?php if (isset($_GET['month']) and isset($_GET['packageId'])): ?>
+                        <br>
+                        <h4><?php
+                        $string = "";
+
+                        if (isset($_GET['month']) and !empty($_GET['packageId'])) {
+                            $qry13 = mysqli_query($connection, "select * from package_view where packageId = '" . $_GET['packageId'] . "'");
+                            $res13 = mysqli_fetch_assoc($qry13);
+                            echo "All payment transactions on " . date("F Y", strtotime($_GET['month'])) . " with the package name: " . $res13['packageName'];
+                        }
+                        else
+                        {
+                            echo "All payment transactions on " . date("F Y", strtotime($_GET['month'])) . "";
+                        }
+
+
+                        echo $string;
+                        ?></h4>
+                        <div class="table-responsive m-t-20">
+                        <table id="myTable" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Payment ID</th>
+                                    <th>Name</th>
+                                    <th>Travel and Tour ID</th>
+                                    <th>Package Name</th>
+                                    <th>Travel Dates</th>
+                                    <th>Booking ID</th>
+                                    <th>Payment Type</th>
+                                    <th>Amount Sent</th>
+                                    <th>Date Sent</th>
+                                    <th>Transaction Code</th>
+                                    <th>Sender</th>
+                                    <th>Remittance</th>
+                                    <th>Proof Image</th>
+                                    <th>Status</th>
+             
+                            
+                                </tr>
+                            </thead>
+                            <tbody>
+                                        <?php
+                                        $qry = mysqli_query($connection, "select * from payment_transaction_view where (departureDate like '%" . $_GET['month'] . "%' or returnDate like '%" . $_GET['month'] . "%')  and packageId like '%" . $_GET['packageId'] ."%'");
+                                        while ($res = mysqli_fetch_assoc($qry)) { ?>
+                                            <tr>
+                                                <td><?php echo $res['paymentTransactionId']; ?></td>
+                                                <td><?php echo $res['firstName'] . " " . $res['middleName'] . " " . $res['lastName']; ?></td>
+                                                <td><?php echo $res['travelAndTourId']; ?></td>
+                                                <td><?php echo $res['packageName']; ?></td>
+                                                <td><?php echo $res['departureDate'] . " to " . $res['returnDate']; ?></td>
+                                                <td><?php echo $res['bookingId']; ?></td>
+                                                <td><?php echo $res['paymentType']; ?></td>
+                                                <td>₱<?php echo number_format($res['amount'],2); ?></td>
+                                                <td><?php echo $res['dateOfPayment']; ?></td>
+                                                <td><?php echo $res['transactionNumber']; ?></td>
+                                                <td><?php echo $res['nameOfSender']; ?></td>
+                                                <td><?php echo $res['paymentMode']." " .$res['nameOfRemittanceOrBank']; ?></td>
+                                                <td>
+                                                    <?php $qry1 = mysqli_query($connection, "select * from payment_transaction_media_view where paymentTransactionId = '" . $res['paymentTransactionId'] . "'"); $res1 = mysqli_fetch_assoc($qry1);
+
+                                                     ?>
+                                                    <a  target="_blank" href="../<?php echo $res1['mediaLocation'] ?>">view image</a>
+                                                </td>
+                                                <td><?php echo $res['paymentStatus']; ?></td>
+                                     
+    
+
+                                            </tr>
+                                        <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <?php endif ?>
+                </div>
                         </div>
                     </div>
                 </div>
@@ -44,119 +154,5 @@ include("includes/header.php");
                 <!-- ============================================================== -->
                 
 
-
+<br><br><br>
 <?php include("includes/footer.php") ?>
-
-<script type="text/javascript">
-
-$(function () {
-    "use strict";
-    // ============================================================== 
-    // Product chart
-    // ============================================================== 
-    Morris.Area({
-        element: 'morris-area-chart2',
-        data: [{
-            period: '2018-05',
-            Bookings: 152000,
-        },{
-            period: '2018-06',
-            Bookings: 230000,
-        },
-        {
-            period: '2018-07',
-            Bookings: 200000,
-        },
-        {
-            period: '2018-08',
-            Bookings: <?php $qry15 = mysqli_query($connection, "SELECT coalesce(sum(amount),0) as result FROM payment_transaction_view WHERE paymentStatus = 'Recieved'"); $res15 = mysqli_fetch_assoc($qry15); echo $res15['result']; ?>,
-        }
-
-        ],
-        xkey: 'period',
-        ykeys: ['Bookings'],
-        labels: ['Bookings Income'],
-        pointSize: 0,
-        fillOpacity: 0.4,
-        pointStrokeColors:['#b4becb', '#01c0c8'],
-        behaveLikeLine: true,
-        gridLineColor: 'rgba(120, 130, 140, 0.13)',
-        lineWidth: 0,
-        smooth: true,
-        hideHover: 'auto',
-        lineColors: ['#b4becb', '#01c0c8'],
-        resize: true
-        
-    });
-   // ============================================================== 
-   // Morris donut chart
-   // ==============================================================       
-    Morris.Donut({
-        element: 'morris-donut-chart',
-        data: [{
-            label: "Reserved - Pending Down Payment",
-            value: <?php $qry15 = mysqli_query($connection, "SELECT COUNT(*) as result FROM booking_view WHERE bookingStatus = 'Reserved - Pending Down Payment'"); $res15 = mysqli_fetch_assoc($qry15); echo $res15['result']; ?>,
-
-        }, {
-            label: "Reserve",
-            value: <?php $qry15 = mysqli_query($connection, "SELECT COUNT(*) as result FROM booking_view WHERE bookingStatus = 'Reserve'"); $res15 = mysqli_fetch_assoc($qry15); echo $res15['result']; ?>,
-        }, {
-            label: "Booked",
-            value: <?php $qry15 = mysqli_query($connection, "SELECT COUNT(*) as result FROM booking_view WHERE bookingStatus = 'Booked'"); $res15 = mysqli_fetch_assoc($qry15); echo $res15['result']; ?>
-        }],
-        resize: true,
-        colors:['#26c6da', '#1976d2', '#ef5350']
-    });
-    // ============================================================== 
-    // sales difference
-    // ==============================================================
-    
-    // ============================================================== 
-    // sparkline chart
-    // ==============================================================
-    var sparklineLogin = function() { 
-       $('#sparklinedash').sparkline([ 0, 5, 6, 10, 9, 12, 4, 9], {
-            type: 'bar',
-            height: '50',
-            barWidth: '2',
-            resize: true,
-            barSpacing: '5',
-            barColor: '#26c6da'
-        });
-         $('#sparklinedash2').sparkline([ 0, 5, 6, 10, 9, 12, 4, 9], {
-            type: 'bar',
-            height: '50',
-            barWidth: '2',
-            resize: true,
-            barSpacing: '5',
-            barColor: '#7460ee'
-        });
-          $('#sparklinedash3').sparkline([ 0, 5, 6, 10, 9, 12, 4, 9], {
-            type: 'bar',
-            height: '50',
-            barWidth: '2',
-            resize: true,
-            barSpacing: '5',
-            barColor: '#03a9f3'
-        });
-           $('#sparklinedash4').sparkline([ 0, 5, 6, 10, 9, 12, 4, 9], {
-            type: 'bar',
-            height: '50',
-            barWidth: '2',
-            resize: true,
-            barSpacing: '5',
-            barColor: '#f62d51'
-        });
-       
-   }
-    var sparkResize;
- 
-        $(window).resize(function(e) {
-            clearTimeout(sparkResize);
-            sparkResize = setTimeout(sparklineLogin, 500);
-        });
-        sparklineLogin();
-});
-
-
-</script>
