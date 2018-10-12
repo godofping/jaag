ba291f18a3e48cc9d89b8322f176154db6c20753
godofping <rexmicrosoft@yahoo.com.ph>
2018-10-12 19:40:02 +0800

    Update dashboard/includes/header.php

--- dashboard/includes/header.php
+++ dashboard/includes/header.php
@@ -117,7 +117,7 @@
                         <!-- ============================================================== -->
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle text-muted text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-message"></i>
-                                <div class="notify">  </div>
+                                <div class="notify"> <span class="heartbit"></span> <span class="point"></span> </div>
                             </a>
                             <div class="dropdown-menu mailbox animated slideInUp">
                                 <ul>
